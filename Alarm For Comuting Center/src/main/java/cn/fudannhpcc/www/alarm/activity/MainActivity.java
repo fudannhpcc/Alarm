bd98645444f91da300f52507c7f1c9b16905f015
Fu Rongtang <rtfu@fudan.edu.cn>
2017-09-16 07:17:44 +0800

    Update

--- Alarm For Comuting Center/src/main/java/cn/fudannhpcc/www/alarm/activity/MainActivity.java
+++ Alarm For Comuting Center/src/main/java/cn/fudannhpcc/www/alarm/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package cn.fudannhpcc.www.alarm;
+package cn.fudannhpcc.www.alarm.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import cn.fudannhpcc.www.alarm.R;
 
 public class MainActivity extends AppCompatActivity {
 
