94671d7168aa658714539f5cf906e668d54846f6
Fu Rongtang <rtfu@fudan.edu.cn>
2017-09-20 18:05:45 +0800

    Update

--- Alarm For Comuting Center/src/main/java/cn/fudannhpcc/www/alarm/commonclass/BootCompletedReceiver.java
+++ Alarm For Comuting Center/src/main/java/cn/fudannhpcc/www/alarm/commonclass/BootCompletedReceiver.java
@@ -4,16 +4,15 @@ package cn.fudannhpcc.www.alarm.commonclass;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
-import cn.fudannhpcc.www.alarm.activity.MainActivity;
+import cn.fudannhpcc.www.alarm.commonclass.MQTTService;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
     @Override
     public void onReceive(Context context, Intent intent) {
-        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED))
-        {
-            Intent newIntent = new Intent(context, MainActivity.class);
-            newIntent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);  //注意，必须添加这个标记，否则启动会失败
-            context.startActivity(newIntent);
+        if (intent.getAction().equals(Intent.ACTION_BOOT_COMPLETED)) {
+            Intent service = new Intent(context, MQTTService.class);
+            service.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+            context.startService(service);
         }
     }
 }
