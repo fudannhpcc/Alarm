0f9ca5fb5460a3566f21d58a917a32b79b53c68a
Fu Rongtang <rtfu@fudan.edu.cn>
2017-09-29 08:56:43 +0800

    Update

--- Alarm For Comuting Center/src/main/java/cn/fudannhpcc/www/alarm/commonclass/Constants.java
+++ Alarm For Comuting Center/src/main/java/cn/fudannhpcc/www/alarm/commonclass/Constants.java
@@ -1,9 +1,9 @@
 package cn.fudannhpcc.www.alarm.commonclass;
 
 public class Constants {
-    public static String MQTT_BROKER_URL = "tcp://fudannhpcc.cn:18883";
-    public static String SUBSCRIBE_TOPIC = "fudannhpcc/alarm/";
+    public static String MQTT_BROKER_URL = "";
+    public static String SUBSCRIBE_TOPIC = "";
     public static String CLIENT_ID = "androidkt";
-    public static String USERNAME = "nhpcc";
-    public static String PASSWORD = "rtfu2002";
+    public static String USERNAME = "";
+    public static String PASSWORD = "";
 }
